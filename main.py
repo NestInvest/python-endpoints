3fdaebe58b1144007b873bf9df130213a7f2ed4c
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2023-09-24 01:08:13 +0300

    ++

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from pydantic.tools import parse_obj_as
-import property.Class
-from property.Class import Property
+from models.property import *
+import os,json
+from typing import List
+import pymongo
+
+fake_db = {
+    "properties": List[Property],
+    "property": Property
+}
+path_to_json = '/mocks/data/property-details/'
+
 
 app = FastAPI()
 
+def search(id, test_list):
+    return [element for element in test_list if element['propertyId'] == id]
+ 
 
+@app.on_event("startup")
+def app_startup():
+    with open("mocks/data/properties-landing-assets.json", "r+") as file:
+        data = json.load(file)
+        properties = parse_obj_as(List[Property], data)   
+        fake_db["properties"] = properties
+        
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -21,10 +40,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/properties")
 def get_properties():
-    properties = new Property()
-    with open("mocks/data/properties-landing-assets.json", "r+") as file:
-        d = json.read(file)
-        
-    return FileResponse("mocks/data/properties-landing-assets.json")
+    return fake_db["properties"]
 
-        
+@app.get("/property/{id}")
+def get_property(id):
+    return search(id, fake_db["properties"])
