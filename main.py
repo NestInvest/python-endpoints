041a3febc3189f54e9199ab980889d4b811ec4fd
taanasaa <said.naghiyev@swisscom.com>
2023-09-24 01:33:35 +0300

    mongo db connection

--- main.py
+++ main.py
@@ -7,41 +7,38 @@ from pydantic.tools import parse_obj_as
 from models.property import *
 import os,json
 from typing import List
-import pymongo
 
-fake_db = {
-    "properties": List[Property],
-    "property": Property
-}
-path_to_json = '/mocks/data/property-details/'
+from dotenv import dotenv_values
+from pymongo import MongoClient
 
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
-def search(id, test_list):
-    return [element for element in test_list if element['propertyId'] == id]
- 
-
 @app.on_event("startup")
 def app_startup():
-    with open("mocks/data/properties-landing-assets.json", "r+") as file:
-        data = json.load(file)
-        properties = parse_obj_as(List[Property], data)   
-        fake_db["properties"] = properties
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
         
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q":q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q":q}
 
-@app.get("/properties")
-def get_properties():
-    return fake_db["properties"]
+# @app.get("/properties")
+# def get_properties():
+#     return fake_db["properties"]
 
-@app.get("/property/{id}")
-def get_property(id):
-    return search(id, fake_db["properties"])
+# @app.get("/property/{id}")
+# def get_property(id):
+#     return search(id, fake_db["properties"])
--- models/property.py
+++ models/property.py
@@ -1,5 +1,6 @@
+import uuid
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Location(BaseModel):
     address: str
@@ -14,17 +15,53 @@ class Contact(BaseModel):
     agentEmail: str
     
 class Property(BaseModel):
-    propertyId: str
-    propertyName: str
-    propertyType: str
-    price: str
-    bedrooms: int
-    bathrooms: int
-    area: str
-    location: Location
-    description: str
-    features: List[str]
-    images: List[str]
-    contact: Contact
+    propertyId: Field(default_factory=uuid.uuid4, alias="_id")
+    propertyName: str = Field(...)
+    propertyType: str = Field(...)
+    price: str = Field(...)
+    bedrooms: int = Field(...)
+    bathrooms: int = Field(...)
+    area: str = Field(...)
+    location: Location = Field(...)
+    description: str = Field(...)
+    features: List[str] = Field(...)
+    images: List[str] = Field(...)
+    contact: Contact = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "propertyId": "12345",
+                "propertyName": "Bla Bla",
+                "propertyType": "Single Family House",
+                "price": "350,000",
+                "bedrooms": 3,
+                "bathrooms": 2,
+                "area": "1,800 sqft",
+                "location": {
+                    "address": "36B Trafalgar st.",
+                    "city": "Villa Park",
+                    "state": "CA",
+                    "zipcode": "12345",
+                    "country": "United States"
+                },
+                "description": "Property located in the countryside between Santa Gertrudis and San Miguel, surrounded by meadows and fruit fields, close to the beaches of the north of the island.\n\n",
+                "features": [
+                    "Open-concept layout",
+                    "Hardwood floors",
+                    "Walk-in closets",
+                    "Two-car garage",
+                    "Central heating and cooling",
+                    "Fenced backyard"
+                ],
+                "images": ["image_url_1.jpg", "image_url_2.jpg", "image_url_3.jpg"],
+                "contact": {
+                    "agentName": "John Smith",
+                    "agentPhone": "(123) 456-7890",
+                    "agentEmail": "john.smith@example.com"
+                }
+            }
+        }
 
     
